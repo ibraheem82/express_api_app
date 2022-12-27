3f12fb7bd3a998244f7a4064027d6caef98efd56
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-12-27 09:51:58 +0100

    some functions were disabled on eslint.

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-object-spread */
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/newline-after-import */
 const fs = require('fs');
 const express = require('express');
 const app = express();
@@ -5,7 +10,6 @@ const app = express();
 // * Middleware
 // it will help in modifying the incoming request data.
 app.use(express.json());
-
 
 // * Creating Route
 // app.get('/', (req, res) => {
@@ -23,47 +27,49 @@ app.use(express.json());
 // });
 
 // * Reading data that will be sent
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-
-// * Route Handler 
+// * Route Handler
 app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    });
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
-
 
 app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body);
+  // console.log(req.body);
 
-    // get the last one, add 1 to the last one.
-    const newId = tours[tours.length - 1].id + 1;
-    // merge the existing objects together
-    const newTour = Object.assign({ id: newId }, req.body);
+  // get the last one, add 1 to the last one.
+  const newId = tours[tours.length - 1].id + 1;
+  // merge the existing objects together
+  const newTour = Object.assign({ id: newId }, req.body);
 
+  tours.push(newTour);
 
-    tours.push(newTour);
-
-    // adding to the json file.
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tours: newTour
-            }
-        });
-    });
+  // adding to the json file.
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+  res.send('Done ');
 });
-
-
 
 // * 🖨 SERVER REALATED STUFF's
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App is running on port ${port}...`)
+  console.log(`App is running on port ${port}...`);
 });
