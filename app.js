bc136b62a439907448a1a6f0700a8028ddea42e2
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-12-29 00:07:49 +0100

    tourRouter and and UserRouter was imported and mounted to thier specific url's

--- app.js
+++ app.js
@@ -2,15 +2,19 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable prefer-object-spread */
 /* eslint-disable import/newline-after-import */
-const fs = require('fs');
 const express = require('express');
+
 const app = express();
+
 // ! morgan is now deprecated.
 // const morgan = require('morgan');
 // can be use to get the actual request that you are on.
 // get the query of the request using morgan.
 // get every about the request.
 const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 // ** (1) MIDDLEWARES
 // ** Creating a custom middleware in express
@@ -37,190 +41,8 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1>Good Evening.</h1>');
 });
 
-// * Using JSON()
-// app.get('/', (req, res) => {
-//     res.status(200).json({message: 'Hello from the server side!', app: 'Natours'})
-// })
-
-// * Post () method
-// app.post('/', (req, res) => {
-//     res.send( 'You can post to this endpoint...')
-// });
-
-// * Reading data that will be sent
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
-);
-// ** (2) ROUTE HANDLERS
-const getAllTours = (req, res) => {
-  console.log(req.requestTime);
-
-  res.status(200).json({
-    status: 'success',
-    requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
-  });
-};
-
-const getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  // If the id is greater than the tours length.
-  // if (id > tours.length) {
-
-  // if the particular tour you are looking for is not found.
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID❌',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-};
-
-const createTour = (req, res) => {
-  // console.log(req.body);
-
-  // get the last one, add 1 to the last one.
-  const newId = tours[tours.length - 1].id + 1;
-  // merge the existing objects together
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  // adding to the json file.
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: {
-          tours: newTour,
-        },
-      });
-    }
-  );
-  res.send('Done ');
-};
-
-const updateTour = (req, res) => {
-  const queryString = req.params.id;
-  // if (req.params.id * 1 > tours.length) {
-  if (queryString * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID❌',
-    });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: `ID ${queryString} was updated`,
-    },
-  });
-};
-
-const deleteTour = (req, res) => {
-  const queryString = req.params.id;
-  // if (req.params.id * 1 > tours.length) {
-  if (queryString * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID❌',
-    });
-  }
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-const getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const updateUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-
-const deleteUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
-  });
-};
-// ! [Getting all tours]
-app.get('/api/v1/tours', getAllTours);
-
-// [:id] is the parameter
-// [?] optional parameter.
-// ! [Getting single tour]
-// * similar to the one below, meaning chaining.
-// app.get('/api/v1/tours/:id', getTour);
-
-// app.post('/api/v1/tours', createTour);
-
-// will send only the updated data.
-// * Patch method
-// app.patch('/api/v1/tours/:id', updateTour);
-
-// * Delete method
-// app.delete('/api/v1/tours/:id', deleteTour);
-
 // * Better way of defining a route that does'nt have id's.
 // ** (3) ROUTES
-const tourRouter = express.Router();
-const userRouter = express.Router();
-
-// app.route('/api/v1/tours').get(getAllTours).post(createTour);
-tourRouter.route('/').get(getAllTours).post(createTour);
-
-// * Route that has ID's.
-// app
-//   .route('/api/v1/tours/:id')
-//   .get(getTour)
-//   .patch(updateTour)
-//   .delete(deleteTour);
-
-tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
-
-// app.route('/api/v1/users').get(getAllUsers).post(createUser);
-// app.route('api/v1/users/:id').get(getUser).patch(updateUser).delete(deleteUser);
-
-userRouter.route('/').get(getAllUsers).post(createUser);
-userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 // Mounting routers on routes
 // MIDDLEWARES
