e583f209828ac08b64b2a1aa8fbaec7ec7458049
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-12-29 05:08:33 +0100

    server codes was removed.

--- app.js
+++ app.js
@@ -52,7 +52,4 @@ app.use('/api/v1/users', userRouter);
 // ** (4) START SERVER
 
 // * 🖨 SERVER REALATED STUFF's
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}...`);
-});
+module.exports = app;
