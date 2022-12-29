d09cb317f1bc2fd0c47bd22982e9b335623193e8
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-12-29 18:14:31 +0100

    environment variables was required and used

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable import/newline-after-import */
 /* eslint-disable no-undef */
+const dotenv = require('dotenv');
+// ** DOTENV in use
+// will be read and saved to the node js environment variable
+dotenv.config({ path: './config.env' });
 const app = require('./app');
-const port = 3000;
+// getting the environment variables.
+
+// ** IN express
+// console.log(app.get('env'));
+
+// ** In node -> js
+// console.log(process.env);
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
 });
