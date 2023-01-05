b8d6804f8fd43231ec204e4d615c2e36095ef05f
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-05 16:37:33 +0100

    Tour model was required.

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -3,24 +3,25 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable no-unused-vars */
 /* eslint-disable prettier/prettier */
-const fs = require('fs');
+// const fs = require('fs');
+const Tour = require("../models/tourModel");
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`Tour ID is: ${val}`);
+// exports.checkID = (req, res, next, val) => {
+//   console.log(`Tour ID is: ${val}`);
 
-  const queryString = req.params.id;
-  if (queryString * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID❌',
-    });
-  }
-  next();
-};
+//   const queryString = req.params.id;
+//   if (queryString * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID❌',
+//     });
+//   }
+//   next();
+// };
 
 // will check the body of the request you are making.
 // used mostly when creating tours.
@@ -39,49 +40,33 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
+  // const tour = tours.find((el) => el.id === id);
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour,
+  //   },
+  // });
 };
 
 exports.createTour = (req, res) => {
-  // console.log(req.body);
-
-  // get the last one, add 1 to the last one.
-  const newId = tours[tours.length - 1].id + 1;
-  // merge the existing objects together
-  const newTour = Object.assign({ id: newId }, req.body);
-
-  tours.push(newTour);
-
-  // adding to the json file.
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
+  res.status(201).json({
         status: 'success',
-        data: {
-          tours: newTour,
-        },
+        // data: {
+        //   tours: newTour,
+        // },
       });
-      // res.send('Done ');
-    }
-  );
+
 };
 
 exports.updateTour = (req, res) => {
