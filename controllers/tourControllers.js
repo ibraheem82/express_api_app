233ef0b1e9b3e7d3992976d4fb488a1e9b19231b
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-05 21:04:47 +0100

    [promise] was used to create data in the database.

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -25,15 +25,15 @@ const Tour = require("../models/tourModel");
 
 // will check the body of the request you are making.
 // used mostly when creating tours.
-exports.checkBody = (req, res, next) => {
-  if (!req.body.name || !req.body.price) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-};
+// exports.checkBody = (req, res, next) => {
+//   if (!req.body.name || !req.body.price) {
+//     return res.status(400).json({
+//       status: 'fail',
+//       message: 'Missing name or price',
+//     });
+//   }
+//   next();
+// };
 exports.getAllTours = (req, res) => {
   console.log(req.requestTime);
 
@@ -59,14 +59,22 @@ exports.getTour = (req, res) => {
   // });
 };
 
-exports.createTour = (req, res) => {
+exports.createTour = async (req, res) => {
+  try {
+    const newTour = await Tour.create(req.body);
   res.status(201).json({
         status: 'success',
-        // data: {
-        //   tours: newTour,
-        // },
+        data: {
+          tours: newTour,
+        },
       });
-
+    
+  } catch (err){
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
