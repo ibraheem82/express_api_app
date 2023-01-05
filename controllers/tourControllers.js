8966ad862200a7f579e9207c3cca6b10ca554fe9
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-05 21:17:34 +0100

    err message was updated

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -72,7 +72,7 @@ exports.createTour = async (req, res) => {
   } catch (err){
     res.status(400).json({
       status: 'fail',
-      message: err
+      message: 'An invalid data was sent ❌'
     });
   }
 };
