6ebb41ef52ae938b9366ec59640bccd0fda87b69
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-08 14:25:42 +0100

    changes was made.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 
-// * Serving stacic files.
+// * Serving static files.
 app.use(express.static(`${__dirname}/public/`));
 app.use((req, res, next) => {
   console.log('Middleware calling');
