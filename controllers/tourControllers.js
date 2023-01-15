a260ae61f34422fcbb6802480c79ba5f64e5d17e
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-15 17:33:45 +0100

    [getMonthlyPlan] was created with alots of queries added to it.

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -271,3 +271,72 @@ exports.getTourStats = async (req, res) => {
     });
   }
 }
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1; // 2021
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+          duration:{$max: '$duration'}
+        }
+      },
+
+      {
+        // is use to add fields
+        $addFields: {month: '$_id'}
+      },
+
+      {
+        // [project] will make the [id] to no longer show up.
+        $project: {
+
+          _id: 0
+        }
+      },
+
+      {
+        // [-1] -> for desending.
+        $sort: {numTourStarts: -1}
+      },
+
+      {
+        // [$limit:] is use to get the numbers of output you want.
+        $limit: 12
+      }
+
+
+
+
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+    
+  }
+}
