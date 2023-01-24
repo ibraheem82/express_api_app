d5e59b437c619b64d1390954e4087271cf90f32f
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-24 07:35:33 +0100

    max and min validators were added for the  [ratingsAverage].

--- models/tourModel.js
+++ models/tourModel.js
@@ -27,11 +27,19 @@ const tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have a difficulty']
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
+      
+    }
   },
   ratingsAverage: {
     type: Number,
-    default: 4.5
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max:[5, 'Rating must be below 5.0'],
+
   },
 
   ratingsQuantity: {
