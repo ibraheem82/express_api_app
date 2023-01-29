3b6b340bcb7fa1bff10a33333fee6ca7a6619bd8
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-29 11:30:14 +0100

    AppError -> class  and globalErrorHandler function was imported all just to handle the global errors.

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 /* eslint-disable prefer-object-spread */
 /* eslint-disable import/newline-after-import */
 const express = require('express');
-const app = express();
 
 // ! morgan is now deprecated.
 // const morgan = require('morgan');
@@ -11,9 +10,14 @@ const app = express();
 // get the query of the request using morgan.
 // get every about the request.
 const morgan = require('morgan');
-
+const AppError = require('./utils/appError')
+const globalErrorHandler = require('./controllers/errorController')
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
+
+const app = express();
+
 
 // ** (1) MIDDLEWARES
 // ** Creating a custom middleware in express
@@ -27,10 +31,7 @@ app.use(express.json());
 
 // * Serving static files.
 app.use(express.static(`${__dirname}/public/`));
-app.use((req, res, next) => {
-  console.log('Middleware calling');
-  next();
-});
+
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -53,7 +54,31 @@ app.get('/', (req, res) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// ** (4) START SERVER
 
+// [app.all] -> will run for all the handlers.
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // });
+
+  // ! Creating an error
+  // const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  // * when the next() is reached it will assume that all other requests are errors and they will all be skipped, and will send the error to the global error handling middleware which will be executed.
+  // * it will skip other middlewares in the stack and goes straight to the error handling middleware.
+  // next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+// * Middleware handlers
+app.use(globalErrorHandler)
+
+
+
+
+// ** (4) START SERVER
 // * 🖨 SERVER REALATED STUFF's
 module.exports = app;
