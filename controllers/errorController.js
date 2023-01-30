9ba5989bfbe19797ccf1626125fe35c63df61668
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-01-30 06:34:46 +0100

    errorController was exported, to show the statusCode in respective to the mongoose built in error validation.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,6 +2,7 @@
 module.exports = (err, req, res, next) => {
   // show us where the error happended
   // console.log(err.stack);
+  // * mongoose error or it statusCode code error message.
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
