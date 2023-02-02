718bb9a8d5aa73f87495372a5a930801d6528f74
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-02 19:28:10 +0100

    errorHandler was provided for each error, also explicitly defining errors that we be catch or thrown both in the production and developer.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,13 +1,52 @@
 /* eslint-disable prettier/prettier */
+
+const sendErrorDev = (err, res) => {
+ res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack
+    });
+}
+
+
+const sendErrorProd = (err, res) => {
+  // * Operational error that we can trust, send message to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+          status: err.status,
+          message: err.message,
+     });
+
+    // Programming Error, : Or other unknown error that we dont want to leak the details  out.
+  } else {
+    // ** Procedures on error
+    // 1) Log error
+    console.error("Error❌", err)
+
+
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    })
+  }
+}
+
 module.exports = (err, req, res, next) => {
   // show us where the error happended
   // console.log(err.stack);
   // * mongoose error or it statusCode code error message.
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  
+  }
+  
 };
+ 
