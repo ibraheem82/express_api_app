bd9e887d7a019e0f77c2895766d23ce7ff5bf6f4
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-23 10:30:42 +0100

    error for duplicate database field was handled and the production error.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,16 +5,18 @@ const AppError = require("../utils/appError");
 
 const handleCastErrorDB = err => {
   // returning our own app error.
-  const message = `Invalid  ${err.path} is passed in: ${err.value}.`;
+  const message = `Invalid  ${err.path} was passed in: ${err.value}.`;
   // doing this with the help of operational error.
   return new AppError(message, 400);
 };
 
 const handleDuplicateFieldNameErrorDb = err => {
   // returning our own app error.
-  const message = ` Duplicate data entries at : ${err.keyValue.name}: ${err.code}.`;
+  // const checkValue = err.keyValue.name.match(/(["'])(\\?.)*?\1/);
+  const message = ` Duplicate fieldvalue :\"${err.keyValue.name}"\ please use another value.`;
+  
   // doing this with the help of operational error.
-  return new AppError(message, 409);
+  return new AppError(message, 400);
 };
 
 
@@ -31,10 +33,13 @@ const sendErrorDev = (err, res) => {
 
 const sendErrorProd = (err, res) => {
   // * Operational error that we can trust, send message to the client
+  // using the operational error to send down message to the client.
   if (err.isOperational) {
     res.status(err.statusCode).json({
-          status: err.status,
-          message: err.message
+            status: err.status,
+            message: err.message,
+            // stack: err.stack
+            
      });
 
     // Programming Error, : Or other unknown error that we dont want to leak the details  out.
@@ -60,15 +65,20 @@ module.exports = (err, req, res, next) => {
   // * mongoose error or it statusCode code error message.
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
+  err.message = err.message || 'error message'
+  err.stack = err.stack || 'error stack'
+
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
-  
   } else if (process.env.NODE_ENV === 'production') {
+    
     let error = {...err};
+    // console.log(error)
     //[handleCastErrorDB(err)] ->  passing the error that mongoose created into the function, will also return a new error created with our (AppError) class and that error will be marked as an operational error, beacuse all our AppError have the isOperational property set to true automatically.
     if (error.kind === 'ObjectId') error = handleCastErrorDB(error);
-    if (error.code === 11000) error = handleDuplicateFieldNameErrorDb(error);
-    // if (error.name === 'CastError') error = handleCastErrorDB(error);
+      // if (error.name === 'CastError') error = handleCastErrorDB(error);
+      if (error.code === 11000) error = handleDuplicateFieldNameErrorDb(error);
+    
     // will be sent to the client
     // handleCastErrorDB -> will be sent to the client.
     sendErrorProd(error, res);
