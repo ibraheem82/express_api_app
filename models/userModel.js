4b89fb3c450fbcd41ad772afde8f2774c915eb30
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-25 18:40:19 +0100

    schema for the user was created with some field and validate was run on the email field to check wether the email that is been provided is a valid email address.

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+     required: [true, 'Please tell use your name.']
+  },
+
+  email: {
+    type: String,
+    required: [true, 'Please provide your email.'],
+    unique:true,
+    lowercase: true,
+    validate:[validator.isEmail]
+  },
+})
