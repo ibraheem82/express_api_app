5f3c80b360c67d7679c7f21534701699f593cf5a
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-25 15:04:39 +0100

    updates.

--- utils/appError.js
+++ utils/appError.js
@@ -8,13 +8,14 @@ class AppError extends Error {
     super(message);
 
     this.statusCode = statusCode;
-    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error'; 
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     // all the errors that we create will basically be operational errors.
-    // using the operationa TO SEND DOWN message to the client at least in production.
-    this.isOperation = true;
+    // using the operational TO SEND DOWN message to the client at least in production.
+    this.isOperational = true;
 
 
     // when a new object is created and the constructor function is called, then that function call is not going to appear in the stack trace and will not pollute it. 
+    // ** captureStackTrace returns a string that represents the location of that particular error in the call. It gives us a stack that helps us to find the location of that error in the code at which new Error() was Called
     Error.captureStackTrace(this, this.constructor)
   }
 }
