965066e5a012e89662011653a06a5b70d51da8de
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-26 09:13:58 +0100

    the user signup was created and exported.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,11 @@
 const User = require('../models/userModel');
+exports.signup = async (req, res, next) => {
+  // To create a new user
+  const newUser = await User.create(req.body)
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser
+    }
+  });
+}
