532d1f20a188098abbce3ee0d1a39bd2d29244e3
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-02-26 09:21:12 +0100

    [authController] was imported and used on the signup.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,12 @@
 /* eslint-disable prettier/prettier */
 const express = require('express');
 const userController = require('../controllers/userControllers');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+
+router.post('/signup', authController.signup)
 router
   .route('/')
   .get(userController.getAllUsers)
