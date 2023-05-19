6005f1acb1adcbdd98d0b4f95afd3825664588af
ibraheem82 <ibraheemomikunle82@gmail.com>
2023-05-19 15:41:37 +0100

    Commit: Implementing user authentication
    In this commit, I implemented user authentication functionality in the user controller file.
    
    Changes:
    - Imported the necessary modules (`jwt`, `User`, `catchAsync`, `AppError`) and the `signToken` function.
    - Implemented the `signup` function to create a new user using the `User.create` method and generate a JWT token using the `signToken` function.
    - Implemented the `login` function to verify the email and password, find the user in the database, and generate a JWT token if the credentials are correct.
    - Implemented the `protect` function as a middleware to check if the user is logged in by validating the JWT token. The token is extracted from the request headers and verified using `jwt.verify`.
    - Added error handling for cases where the token is missing or invalid.
    
    These changes lay the foundation for user authentication and token-based authorization in the application.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,16 +1,14 @@
+/* eslint-disable prettier/prettier */
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-const signToken = id => {
-  return jwt.sign(
-    { id }, process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES_IN }
-  );
-}
-
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 exports.signup = catchAsync(async (req, res, next) => {
   // To create a new user
@@ -18,8 +16,9 @@ exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
+    // eslint-disable-next-line prettier/prettier
     password: req.body.password,
-    passwordConfirm: req.body.passwordConfirm
+    passwordConfirm: req.body.passwordConfirm,
   });
   const token = signToken(newUser._id);
 
@@ -27,17 +26,17 @@ exports.signup = catchAsync(async (req, res, next) => {
     status: 'success',
     token,
     data: {
-      user: newUser
-    }
+      user: newUser,
+    },
   });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
   // created variables from the body object using destructing
   const { email, password } = req.body;
-// * 1) Check if email and password.
+  // * 1) Check if email and password.
   if (!email || !password) {
-    return next(new AppError('Please provide email and password.', 400))
+    return next(new AppError('Please provide email and password.', 400));
   }
 
   // * 2) Check if user exists && password is correct.
@@ -47,41 +46,43 @@ exports.login = catchAsync(async (req, res, next) => {
   // const correct = await user.correctPassword(password, user.password);
   // console.log(user);
 
-
   // if the user does not exist it will not run the next code which is the -> await user.correctPassword
   if (!user || !(await user.correctPassword(password, user.password))) {
     // [401] -> unathourised
-    return next(new AppError('Incorrect email or password', 401))
+    return next(new AppError('Incorrect email or password', 401));
   }
-// * 3) If everything ok, send token to client
+  // * 3) If everything ok, send token to client
   // token will be generated and send back to the user.
   const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
-    token
+    token,
   });
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
   // * 1) Getting the token and check if it there.
   let token;
-  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
     token = req.headers.authorization.split(' ')[1];
   }
 
-
   if (!token) {
-    return next(new AppError('You are not logged in! please log in to get access.', 401));
+    return next(
+      new AppError('You are not logged in! please log in to get access.', 401)
+    );
   }
   // * 2) Validate token -> (verification token)
   // the decoded payload or data from the json web token.
   const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  console.log(decoded)
-  
+  console.log(decoded);
+
   // * 3) Check if the user that is trying to access the route still exists.
 
   // * 4) Check if the user changed the password after the JWT(token) was issued.
-
 
   next();
 });
